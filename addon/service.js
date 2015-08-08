45bdc10677b2bdfd089c55c8edd2e13c49b50240
Benjamin Balazs <benjaminbalazs@gmail.com>
2015-08-09 01:48:04 +0400

    1

---
+++ addon/service.js
@@ -0,0 +1,58 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+
+	//
+
+	log : function(message, details) {
+
+		this.send('log', { message: this.prefix + ' ' + message, details: details });
+
+	},
+
+	error : function(error) {
+
+		this.send('error', { error: this.prefix + ' ' + JSON.stringify(error) });
+
+	},
+
+	//
+
+	send : function(method, data) {
+
+		return new Ember.RSVP.Promise(function(resolve, reject) {
+
+			$.ajax(
+		 			{
+					    url: self.api_namespace + "/" + method, 
+					    type: 'POST', 
+					    contentType: 'application/vnd.api+json',
+					    data: JSON.stringify(data),
+					    dataType: 'json',
+					    crossDomain: true,
+					    headers: {
+					    	'Access-Control-Allow-Origin': '*'
+					    }
+					}
+			).done(function(data) { // IF SUCCESS
+				resolve(data);
+			}).fail(function(error) { // IF FAILED
+				reject(error);
+			});
+
+		});
+
+	},
+
+	//
+
+	initialize : Ember.on('init', function() {
+
+		var config = this.container.lookupFactory('config:environment');
+		
+		this.api_namespace =  "/" + config.APP.api_namespace + "/logger";
+		this.prefix = config.modulePrefix;
+
+	})
+
+});
---
+++ app/initializers/logger.js
@@ -0,0 +1,17 @@
+import LoggerService from 'sq-ember-logger/service';
+
+export function initialize (container, app) {
+
+	app.register('logger:main', LoggerService);
+
+    app.inject('route', 'logger', 'logger:main');
+    app.inject('adapter', 'logger', 'logger:main');
+    app.inject('component', 'logger', 'logger:main');
+    app.inject('controller', 'logger', 'logger:main');
+
+}
+
+export default {
+    name: 'logger',
+    initialize: initialize
+}
